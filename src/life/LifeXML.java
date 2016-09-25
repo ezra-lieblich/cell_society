6f10a5e8884820d4cd6ba70d4f72bafe2b1dd928
CHRISTOPHER LU <cjl46@duke.edu>
2016-09-25 15:50:27 -0400

    Fixes header issues in LifeXML, LifeXMLFactory. Includes XML file for SpreadofFire with probCatch value. Implements the TreeXML and TreeXMLFactory classes.

--- src/life/LifeXML.java
+++ src/life/LifeXML.java
@@ -6,8 +6,8 @@ public class LifeXML extends Simulation {
 	private String percentAlive;
 	private String percentDead;
 	
-	public LifeXML (String simulationName, String simulationTitle, String simulationAuthor, String percentX, String percentY, String pAlive, String pDead) {
-		super(simulationName, simulationTitle, simulationAuthor, percentX, percentY);
+	public LifeXML (String simulationName, String simulationTitle, String simulationAuthor, String xSize, String ySize, String pAlive, String pDead) {
+		super(simulationName, simulationTitle, simulationAuthor, xSize, ySize);
 		percentAlive = pAlive;
 		percentDead = pDead;
 	}
@@ -27,8 +27,8 @@ public class LifeXML extends Simulation {
               .append("simulationName='").append(getName()).append("', ")
               .append("simulationTitle='").append(getTitle()).append("', ")
               .append("simulationAuthor='").append(getAuthor()).append("', ")
-              .append("percentX='").append(getXSize()).append("', ")
-              .append("percentYSize='").append(getYSize()).append("'. ")
+              .append("xSize='").append(getXSize()).append("', ")
+              .append("ySize='").append(getYSize()).append("'. ")
               .append("percentAlive='").append(getPercentAlive()).append("', ")
               .append("percentDead='").append(getPercentDead()).append("', ")
               .append('}');
