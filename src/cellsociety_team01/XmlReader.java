c3885bc89e27764056a0bbd0c844f6662f0f2518
CHRISTOPHER LU <cjl46@duke.edu>
2016-09-30 16:19:22 -0400

    Changed name of grid creater from getGrid to makeGrid.

--- src/cellsociety_team01/XmlReader.java
+++ src/cellsociety_team01/XmlReader.java
@@ -26,9 +26,6 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class XmlReader {
-	// Width of grid array, height of grid array, type of sim, data in each cell
-	// of the grid
-	private BasicFiniteGrid grid;
 	// Reset DOCUMENT_BUILDER before every parse
 	private static final DocumentBuilder DOCUMENT_BUILDER = getDocumentBuilder();
 	private static Element root;
@@ -42,7 +39,6 @@ public class XmlReader {
 	}
 
 	public Element getRootElement(File xmlFile) {
-		System.out.println(xmlFile.getAbsolutePath());
 		try {
 			DOCUMENT_BUILDER.reset();
 			Document xmlDocument = DOCUMENT_BUILDER.parse(xmlFile.getAbsolutePath());
@@ -63,21 +59,15 @@ public class XmlReader {
 	
 	 /**
      * Get the text value of a node.
-     * <p>
      * Assumes you want the textValue of the first node with this tagName.
-     * </p>
-     * 
-     * Why might it not be good design to include this and getAttribute in this class?
-     * What happens when I need more transformation methods?
      */
     public String getTextValue (Element root, String tagName) {
         NodeList nodeList = root.getElementsByTagName(tagName);
         if (nodeList != null && nodeList.getLength() > 0) {
         	String a = nodeList.item(0).getTextContent();
-            return nodeList.item(0).getTextContent();
+            return a;
         }
         else {
-            // BUGBUG: return empty string or null, is it an error to not find the text value?
             return "";
         }
     }
@@ -108,7 +98,7 @@ public class XmlReader {
 		return bounds;
 	}
 	
-	private void makeFactories() {
+	public void makeFactories() {
 		if (Sim.equals("Game Of Life")) {
 			makeLife();
 		}
@@ -126,7 +116,7 @@ public class XmlReader {
 	private void makeLife() {
 		LifeFactory life= new LifeFactory(shape, bounds, rows, columns);
 		Double Alive = Double.parseDouble(getTextValue(root, "percentAlive"));
-		return life.getGrid(Alive);
+		return life.makeGrid(Alive);
 	}
 	
 	private void makeFire() {
@@ -134,7 +124,7 @@ public class XmlReader {
 		Double Tree = Double.parseDouble(getTextValue(root, "percentTree"));
 		Double Burn = Double.parseDouble(getTextValue(root, "percentBurn"));
 		Double probCatch = Double.parseDouble(getTextValue(root, "probCatch"));
-		return fire.getGrid(Tree, Burn, probCatch);
+		return fire.makeGrid(Tree, Burn, probCatch);
 	}
 	
 	private void makeWaTor() {
@@ -144,7 +134,7 @@ public class XmlReader {
 		int fishRep = Integer.parseInt(getTextValue(root, "fishReproduce"));
 		int sharkDea = Integer.parseInt(getTextValue(root, "sharkDeath"));
 		int sharkRep = Integer.parseInt(getTextValue(root, "sharkReproduce"));
-		return wator.getGrid(Fish, Shark, fishRep, sharkDea, sharkRep);
+		return wator.makeGrid(Fish, Shark, fishRep, sharkDea, sharkRep);
 	}
 	
 	private void makeXO() {
@@ -152,7 +142,7 @@ public class XmlReader {
 		Double perX = Double.parseDouble(getTextValue(root, "percentX"));
 		Double perO = Double.parseDouble(getTextValue(root, "percentO"));
 		Double perSim = Double.parseDouble(getTextValue(root, "similarPercentage"));
-		return xo.getGrid(perX, perO, perSim);
+		return xo.makeGrid(perX, perO, perSim);
 	}
 	
 //	private BasicFiniteGrid initiateLife() {
@@ -189,15 +179,6 @@ public class XmlReader {
 //		return temp;
 //	}
 //	
-//	public int getFishReproduce(){
-//		return Integer.parseInt(factory.getTextValue(root, "fishReproduce"));
-//	}
-//	public int getSharkDeath(){
-//		return Integer.parseInt(factory.getTextValue(root, "sharkDeath"));
-//	}
-//	public int getSharkReproduce(){
-//		return Integer.parseInt(factory.getTextValue(root, "sharkReproduce"));
-//	}
 //	
 //	private BasicFiniteGrid initiateWaTor() {
 //		String strColumns = factory.getTextValue(root, "XGridSize");
@@ -253,12 +234,6 @@ public class XmlReader {
 //		}
 //		return temp;
 //	}
-
-//	private BasicFiniteGrid initiateLife() {
-//		LifeXMLFactory makeLife = new LifeXMLFactory();
-//		BasicFiniteGrid temp = makeLife.makeGrid(root);
-//		return temp;
-//	}
 //
 //	private BasicFiniteGrid initiateTree() {
 //		String strRows = factory.getTextValue(root, "XGridSize");
@@ -308,10 +283,6 @@ public class XmlReader {
 //			}
 //		}
 //		return temp;
-//	}
-//
-//	public double getPercentSimilar() {
-//		return Double.parseDouble(factory.getTextValue(root, "similarPercentage"));
 //	}
 //
 //	private BasicFiniteGrid initiateXO() {
